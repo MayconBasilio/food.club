795847c2bf726c540fbc3e83873ad0bdd5440a45
MayconBasilio <142848115+MayconBasilio@users.noreply.github.com>
2024-11-26 19:41:40 -0300

    Atualização2

--- client/src/components/MappingImage.tsx
+++ client/src/components/MappingImage.tsx
@@ -1,13 +1,15 @@
 import { employeesFoto } from '../data/mockData'
 
 interface IProps {
-    image: [object];
+    image: object[];
 }
 
 export const MappingImage = ({image}:IProps) => {
     return (
-        {employeesFoto.map(({image}) => {
-
-        })}
+        <div>
+      {employeesFoto.map((employee, index) => (
+        <img key={index} src={employee.image} alt={`Employee ${index}`} />
+      ))}
+    </div>
     )
 }
