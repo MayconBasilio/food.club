eb6e223f6086f11d96327f4f8b0f52c14ddd5a48
MayconBasilio <142848115+MayconBasilio@users.noreply.github.com>
2024-11-26 19:32:18 -0300

    Atualização

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,10 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-	cors({
+	cors(/*{
 		origin: "http://localhost:5173",
 		credentials: true,
-	})
+	}*/)
 );
 app.use(cookieParser());
 
